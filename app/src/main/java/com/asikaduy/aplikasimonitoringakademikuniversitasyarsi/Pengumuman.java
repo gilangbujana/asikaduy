50fe6f9255226728eda411ae486ebc8e996f881c
gilangbujana <noviangilang@gmail.com>
2020-01-02 10:02:49 +0700

    [EDIT] edit Pengumuman.java change pick date with datepickerdialog [EDIT] edit Profile.java when update button click, it call update2 inner-class

--- app/src/main/java/com/asikaduy/aplikasimonitoringakademikuniversitasyarsi/Pengumuman.java
+++ app/src/main/java/com/asikaduy/aplikasimonitoringakademikuniversitasyarsi/Pengumuman.java
@@ -2,6 +2,7 @@ package com.asikaduy.aplikasimonitoringakademikuniversitasyarsi;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -15,6 +16,7 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,9 +28,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 public class Pengumuman extends Activity {
 	String ip="";
@@ -54,6 +58,9 @@ String gambar="";
 	private static final String TAG_status = "status";
 	private static final String TAG_keterangan = "keterangan";
 
+	private DatePickerDialog datePickerDialog;
+	private SimpleDateFormat dateFormatter;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -73,8 +80,57 @@ String gambar="";
 			new get().execute();
 		}
 
+		dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+
+		txttanggal.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				showDateDialog();
+			}
+		});
 
 
+
+	}
+
+
+	private void showDateDialog(){
+
+		/**
+		 * Calendar untuk mendapatkan tanggal sekarang
+		 */
+		Calendar newCalendar = Calendar.getInstance();
+
+		/**
+		 * Initiate DatePicker dialog
+		 */
+		datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+			@Override
+			public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+				/**
+				 * Method ini dipanggil saat kita selesai memilih tanggal di DatePicker
+				 */
+
+				/**
+				 * Set Calendar untuk menampung tanggal yang dipilih
+				 */
+				Calendar newDate = Calendar.getInstance();
+				newDate.set(year, monthOfYear, dayOfMonth);
+
+				/**
+				 * Update TextView dengan tanggal yang kita pilih
+				 */
+				txttanggal.setText(""+dateFormatter.format(newDate.getTime()));
+			}
+
+		},newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+		/**
+		 * Tampilkan DatePicker dialog
+		 */
+		datePickerDialog.show();
 	}
 
 	//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
